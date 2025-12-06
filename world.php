47c9dbdc854abfb2c5b04969c788d9a31aa1c1d4
Zakyla Petrie <zakylapetrie@gmail.com>
2025-12-05 20:14:24 -0500

    Exercise 4 - Display Countries in Table

--- world.php
+++ world.php
@@ -20,8 +20,23 @@ $stmt->execute(['country' => $searchTerm]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['name']); ?></td>
+            <td><?= htmlspecialchars($row['continent']); ?></td>
+            <td><?= htmlspecialchars($row['independence_year']); ?></td>
+            <td><?= htmlspecialchars($row['head_of_state']); ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
